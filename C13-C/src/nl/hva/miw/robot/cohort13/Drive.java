d34c81ec8f7869ca22c12ad9d0f14a3f79276411
J.M. Slot <jeroen.slot@hva.nl>
2018-10-17 11:03:24 +0200

    latest features

--- C13-C/src/nl/hva/miw/robot/cohort13/Drive.java
+++ C13-C/src/nl/hva/miw/robot/cohort13/Drive.java
@@ -1,22 +1,20 @@
 package nl.hva.miw.robot.cohort13;
 
-import lejos.hardware.Sound;
 import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
 
-public class DriveForward {
-
-	
-	public void geefGas() {
+public class Drive {
 
     UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
     UnregulatedMotor motorD = new UnregulatedMotor(MotorPort.D);
-    motorA.setPower(100);
+
+
+public void goForward() {
+	motorA.setPower(100);
     motorD.setPower(100);
-	Sound.twoBeeps(); 
     motorA.forward();
     motorD.forward();
 	
-	}
+}
 	
 }
--- C13-C/src/nl/hva/miw/robot/cohort13/Marvin.java
+++ C13-C/src/nl/hva/miw/robot/cohort13/Marvin.java
@@ -25,27 +25,57 @@ public class Marvin {
 	
 	public static void main(String[] args) {
 		Marvin marvin = new Marvin();
-		marvin.startMotor();
 	
 		marvin.run();
+	
 		
-		marvin.startMotor();
+		//marvin.startMotor();
+//	
+//		
+//		marvin.startMotor();
+		
+		
+		
 	}
 	
 	private void run() {
 		TextLCD display = brick.getTextLCD();
 		display.drawString("Welkom", 0, 3);
-
 		display.drawString("Team Charlie!", 0, 4);		
+		
+		
+		
+		
+		DriveForwardTester driveForwardTester = new DriveForwardTester();
+		driveForwardTester.testDriveForward(this);
+		
+//		driveForwardTester.driveFast(this);
+//		driveForwardTester.driveSlow(this);
+//		driveForwardTester.stopRobot(this);
+		
+		
+		
 		ColorSensorTester colorSensorTester = new ColorSensorTester();
-		colorSensorTester.test(this);
+//		colorSensorTester.test(this);
 		
 		Sound.buzz();
 		Sound.twoBeeps(); 
 
 		
+		driveForwardTester.driveFast(this);
+		Sound.twoBeeps(); 
+		driveForwardTester.stopRobot(this);
+		
+		
 		TouchSensorTester touchSensorTester = new TouchSensorTester();
-		touchSensorTester.test(this);
+	//	touchSensorTester.test(this);
+			
+		Sound.playTone(5, 10000);
+	
+		System.out.println("Test complete....");
+		
+		
+		
 		
 		waitForKey(Button.ENTER);
 	}
@@ -64,15 +94,27 @@ public class Marvin {
 	
 	public void startMotor() {
 		
-        UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
-        UnregulatedMotor motorD = new UnregulatedMotor(MotorPort.D);
-        motorA.setPower(100);
-        motorD.setPower(100);
+		UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
+		UnregulatedMotor motorC = new UnregulatedMotor(MotorPort.C);
+        motorB.setPower(100);
+		motorC.setPower(50);
+		
+	    motorC.forward();
+      motorB.forward();
 		Sound.twoBeeps(); 
-        motorA.forward();
-        motorD.forward();
+		
+//		
+//        UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
+//        UnregulatedMotor motorD = new UnregulatedMotor(MotorPort.D);
+//        motorA.setPower(100);
+//        motorD.setPower(100);
+//        motorA.forward();
+//        motorD.forward();
+	
+     
 
 	}
+	
 	
 	
 }
